367d215f2458d20af6ca5b0805d660935993e7b1
SPS-JPNPahang <sektorpengurusansekolahpahang@gmail.com>
2025-11-23 23:18:51 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,28 +3,27 @@
 // Setting global untuk frontend PIBG @ SPS
 // --------------------------------------------------
 
+const ORIGINAL_URL = "https://script.google.com/macros/s/AKfycbw0Oj9cSnlqbWO0sNFzFAdxeGx_1hMhOjJfCAF6AID672svCkBhPqonnqkw07mpmc_1Ug/exec";
+
 const CONFIG = {
     SYSTEM_NAME: "PIBG @ SPS",
     VERSION: "1.0.0",
-
-    // MASUKKAN URL WEB APP APPS SCRIPT ANDA DI SINI
-    WEBAPP_URL: "https://script.google.com/macros/s/AKfycbw0Oj9cSnlqbWO0sNFzFAdxeGx_1hMhOjJfCAF6AID672svCkBhPqonnqkw07mpmc_1Ug/exec",
-    // Option 1
-    "https://cors-anywhere.herokuapp.com/" + ORIGINAL_URL,
     
-    // Option 2
-    // "https://api.allorigins.win/raw?url=" + encodeURIComponent(ORIGINAL_URL)
+    // PILIH SALAH SATU PROXY (uncomment 1 line sahaja):
     
-    // Option 3
-    // "https://corsproxy.io/?" + ORIGINAL_URL
-
+    // Option 1: CORS Anywhere
+    WEBAPP_URL: "https://cors-anywhere.herokuapp.com/" + ORIGINAL_URL,
+    
+    // Option 2: AllOrigins
+    // WEBAPP_URL: "https://api.allorigins.win/raw?url=" + encodeURIComponent(ORIGINAL_URL),
+    
+    // Option 3: CorsProxy.io
+    // WEBAPP_URL: "https://corsproxy.io/?" + ORIGINAL_URL,
+    
     TOKEN_KEY: "PIBG_TOKEN",
     ROLE_KEY: "PIBG_ROLE",
-
-    REQUEST_TIMEOUT: 15000 // 15s
+    REQUEST_TIMEOUT: 15000
 };
 
 // export ke global
 window.CONFIG = CONFIG;
-
-
