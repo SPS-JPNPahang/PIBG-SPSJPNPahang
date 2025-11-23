dbd50b8950dfd0a5fb78537b489ed2125258dd1d
SPS-JPNPahang <sektorpengurusansekolahpahang@gmail.com>
2025-11-23 23:16:21 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -9,6 +9,14 @@ const CONFIG = {
 
     // MASUKKAN URL WEB APP APPS SCRIPT ANDA DI SINI
     WEBAPP_URL: "https://script.google.com/macros/s/AKfycbw0Oj9cSnlqbWO0sNFzFAdxeGx_1hMhOjJfCAF6AID672svCkBhPqonnqkw07mpmc_1Ug/exec",
+    // Option 1
+    "https://cors-anywhere.herokuapp.com/" + ORIGINAL_URL
+    
+    // Option 2
+    "https://api.allorigins.win/raw?url=" + encodeURIComponent(ORIGINAL_URL)
+    
+    // Option 3
+    "https://corsproxy.io/?" + ORIGINAL_URL
 
     TOKEN_KEY: "PIBG_TOKEN",
     ROLE_KEY: "PIBG_ROLE",
@@ -18,3 +26,4 @@ const CONFIG = {
 
 // export ke global
 window.CONFIG = CONFIG;
+
