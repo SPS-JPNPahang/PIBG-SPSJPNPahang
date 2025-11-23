9e8a4ae01bfb9b0e78b2f79b8e412c557805bf39
SPS-JPNPahang <sektorpengurusansekolahpahang@gmail.com>
2025-11-23 23:39:57 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -9,22 +9,12 @@ const CONFIG = {
     SYSTEM_NAME: "PIBG @ SPS",
     VERSION: "1.0.0",
     
-    // PILIH SALAH SATU PROXY (uncomment 1 line sahaja):
-    
-    // Option 1: CORS Anywhere
-    //WEBAPP_URL: "https://cors-anywhere.herokuapp.com/" + ORIGINAL_URL,
-    
-    // Option 2: AllOrigins
-    // WEBAPP_URL: "https://api.allorigins.win/raw?url=" + encodeURIComponent(ORIGINAL_URL),
-    
-    // Option 3: CorsProxy.io
-    WEBAPP_URL: "https://corsproxy.io/?" + ORIGINAL_URL,
+    // Direct URL (no proxy)
+    WEBAPP_URL: "https://script.google.com/macros/s/AKfycbw0Oj9cSnlqbWO0sNFzFAdxeGx_1hMhOjJfCAF6AID672svCkBhPqonnqkw07mpmc_1Ug/exec",
     
     TOKEN_KEY: "PIBG_TOKEN",
     ROLE_KEY: "PIBG_ROLE",
     REQUEST_TIMEOUT: 15000
 };
 
-// export ke global
 window.CONFIG = CONFIG;
-
